f7b21b588961b547bbbf8c3b6a3a95a53e56b514
daffabaihaqi <baihaqidaff@gmail.com>
2024-04-26 11:22:09 +0700

    delete all previous file

--- client/src/utils/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const apiRequest = axios.create({
-    baseURL : "http://localhost:3000/"
-})
-
-export const serverRequest = axios.create({
-    baseURL : ""
-});
